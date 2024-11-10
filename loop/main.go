eb02617a48970881e97889bce78930e9ca3d317f
David England <david.england.de@googlemail.com>
2024-11-10 07:42:47 +0000

    Import sudoku from .csv file
    Note: error handling is very much TO DO

--- loop/main.go
+++ loop/main.go
@@ -2,10 +2,34 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"solver-zero/lib"
+	"strconv"
+	"strings"
 )
 
 func main() {
 	fmt.Println("Hello universe.")
-	fmt.Println(lib.Sudoku{})
+	fmt.Println(importSudoku("C:\\Users\\david\\downloads\\test_doc.txt").Grid)
+}
+
+func importSudoku(filepath string) lib.Sudoku {
+	out, _ := os.ReadFile(filepath)
+	return parseCSV(string(out))
+}
+
+func parseCSV(csv string) (suduko lib.Sudoku) {
+	lines := strings.Split(csv, "\n")
+
+	for i := 0; i < 9; i++ {
+		cells := strings.Split(lines[i], ",")
+
+		for j := 0; j < 9; j++ {
+			if len(cells[j]) > 0 {
+				suduko.Grid[i][j], _ = strconv.Atoi(cells[j])
+			}
+		}
+	}
+
+	return suduko
 }
