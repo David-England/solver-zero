022fd63e53ff776f8640aea9ec79d900d3b50b4b
David England <david.england.de@googlemail.com>
2024-11-11 08:02:26 +0000

    Implement GetSubs()

--- lib/sudoku.go
+++ lib/sudoku.go
@@ -8,7 +8,17 @@ type Sub struct {
 	Grid [3][3]int
 }
 
-func (sud Sudoku) GetSubs() [3][3]Sub {
-	subs := [3][3]Sub{}
-	return subs
+func (suduko Sudoku) GetSubs() (subs [3][3]Sub) {
+	for subRow := 0; subRow < 3; subRow++ {
+		for subCol := 0; subCol < 3; subCol++ {
+			for cellRow := 0; cellRow < 3; cellRow++ {
+				for cellCol := 0; cellCol < 3; cellCol++ {
+					subs[subRow][subCol].Grid[cellRow][cellCol] =
+						suduko.Grid[3*subRow+cellRow][3*subCol+cellCol]
+				}
+			}
+		}
+	}
+
+	return
 }
