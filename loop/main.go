4384f7f98b56940497bb04bdbb1c246dab308722
David England <david.england.de@googlemail.com>
2024-11-14 08:47:10 +0000

    Add error handling to loop module
    Note: the code appears to not handle \r characters anymore. Fix inbound.

--- loop/main.go
+++ loop/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"log"
 	"os"
 	"solver-zero/lib"
 	"solver-zero/lib/logics"
@@ -10,36 +12,85 @@ import (
 )
 
 func main() {
-	sud := importSudoku(os.Args[1])
+	sud, importErr := importSudoku(os.Args[1])
+
+	if importErr != nil {
+		log.Fatal(importErr)
+	}
 
 	var logics []lib.ILogic = []lib.ILogic{
 		&logics.EliminateNumbersLogic{Sudoku: &sud},
 	}
 
 	fmt.Println(sud.Grid)
-	for isChanged := true; isChanged; {
-		isChanged, _ = lib.RunStep(logics)
+	if runErr := run(logics); runErr != nil {
+		log.Fatal(runErr)
 	}
 	fmt.Println(sud.Grid)
 }
 
-func importSudoku(filepath string) lib.Sudoku {
-	out, _ := os.ReadFile(filepath)
-	return parseCSV(string(out))
-}
+func run(logics []lib.ILogic) (runError error) {
+	for isChanged := true; isChanged; {
+		isChanged, runError = lib.RunStep(logics)
 
-func parseCSV(csv string) (suduko lib.Sudoku) {
-	lines := strings.Split(csv, "\n")
-
-	for i := 0; i < 9; i++ {
-		cells := strings.Split(lines[i], ",")
-
-		for j := 0; j < 9; j++ {
-			if len(cells[j]) > 0 {
-				suduko.Grid[i][j], _ = strconv.Atoi(cells[j])
-			}
+		if runError != nil {
+			return
 		}
 	}
 
 	return
+}
+
+func importSudoku(filepath string) (lib.Sudoku, error) {
+	contents, err := os.ReadFile(filepath)
+
+	if err != nil {
+		return lib.Sudoku{}, err
+	}
+
+	return parseCSV(string(contents))
+}
+
+func parseCSV(csv string) (lib.Sudoku, error) {
+	lines := strings.Split(csv, "\n")
+	var sudoku lib.Sudoku
+
+	if len(lines) < 9 {
+		return lib.Sudoku{}, errors.New("sudoku provided has fewer than 9 rows")
+	}
+
+	for i := 0; i < 9; i++ {
+		var err error
+
+		sudoku.Grid[i], err = parseLine(lines[i])
+
+		if err != nil {
+			return lib.Sudoku{}, err
+		}
+	}
+
+	return sudoku, nil
+}
+
+func parseLine(line string) ([9]int, error) {
+	cells := strings.Split(line, ",")
+	row := [9]int{}
+
+	if len(cells) < 9 {
+		return [9]int{}, errors.New("one of the lines has fewer than 9 cells")
+	}
+
+	for j := 0; j < 9; j++ {
+		var parseErr error
+
+		if len(cells[j]) > 0 {
+			row[j], parseErr = strconv.Atoi(cells[j])
+		}
+
+		if parseErr != nil {
+			return [9]int{}, parseErr
+		}
+	}
+
+	return row, nil
 }
