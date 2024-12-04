d1eb38da936cd3a651884b73782a5059d401a2be
David England <david.england.de@googlemail.com>
2024-12-04 09:34:45 +0000

    Add unit tests for CandidateX() getters under empty scenario

--- lib/pencil/pencil_marks_test.go
+++ lib/pencil/pencil_marks_test.go
@@ -1,6 +1,7 @@
 package pencil
 
 import (
+	"slices"
 	"solver-zero/lib"
 	"testing"
 )
@@ -157,5 +158,142 @@ func TestBanNothingForEmpty(t *testing.T) {
 				}
 			}
 		}
+	}
+}
+
+func TestCandidateNumbersForEmpty(t *testing.T) {
+	// Arrange
+	sut := PencilMarks{}
+	sudoku := lib.Sudoku{
+		Grid: [9][9]int{
+			{0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0},
+		},
+	}
+	sut.EliminateOptions(&sudoku)
+
+	// Act
+	candidates := sut.CandidateNumbers(0, 0)
+
+	// Assert
+	for num := 1; num <= 9; num++ {
+		if !slices.Contains(candidates, num) {
+			t.Fatalf("every number should be a candidate for an empty sudoku, but %v was not", num)
+		}
+	}
+	if len(candidates) != 9 {
+		t.Fatalf("should be 9 candidate numbers for empty sudoku, but was %v", len(candidates))
+	}
+}
+
+func TestCandidatesInRowForEmpty(t *testing.T) {
+	// Arrange
+	sut := PencilMarks{}
+	row := 0
+	sudoku := lib.Sudoku{
+		Grid: [9][9]int{
+			{0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0},
+		},
+	}
+	sut.EliminateOptions(&sudoku)
+
+	// Act
+	candidates := sut.CandidateCellsInRow(row, 4)
+
+	// Assert
+	for i := 0; i < 9; i++ {
+		cell := lib.Coords{RowIndex: row, ColumnIndex: i}
+		if !slices.Contains(candidates, cell) {
+			t.Fatalf("every cell should be a candidate for an empty row, but %v was not", cell)
+		}
+	}
+	if len(candidates) != 9 {
+		t.Fatalf("should be 9 candidate cells for empty row, but was %v", len(candidates))
+	}
+}
+
+func TestCandidatesInColForEmpty(t *testing.T) {
+	// Arrange
+	sut := PencilMarks{}
+	col := 0
+	sudoku := lib.Sudoku{
+		Grid: [9][9]int{
+			{0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0},
+		},
+	}
+	sut.EliminateOptions(&sudoku)
+
+	// Act
+	candidates := sut.CandidateCellsInColumn(col, 4)
+
+	// Assert
+	for i := 0; i < 9; i++ {
+		cell := lib.Coords{RowIndex: i, ColumnIndex: col}
+		if !slices.Contains(candidates, cell) {
+			t.Fatalf("every cell should be a candidate for an empty column, but %v was not", cell)
+		}
+	}
+	if len(candidates) != 9 {
+		t.Fatalf("should be 9 candidate cells for empty column, but was %v", len(candidates))
+	}
+}
+
+func TestCandidatesInSubForEmpty(t *testing.T) {
+	// Arrange
+	sut := PencilMarks{}
+	subRow, subCol := 2, 2
+	sudoku := lib.Sudoku{
+		Grid: [9][9]int{
+			{0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0},
+		},
+	}
+	sut.EliminateOptions(&sudoku)
+
+	// Act
+	candidates := sut.CandidateCellsInSubgrid(subRow, subCol, 4)
+
+	// Assert
+	for row := 3 * subRow; row < 3*(subRow+1); row++ {
+		for col := 3 * subCol; col < 3*(subCol+1); col++ {
+			cell := lib.Coords{RowIndex: row, ColumnIndex: col}
+			if !slices.Contains(candidates, cell) {
+				t.Fatalf("every cell should be a candidate for an empty subgrid,"+
+					"but %v was not", cell)
+			}
+		}
+	}
+	if len(candidates) != 9 {
+		t.Fatalf("should be 9 candidate cells for empty subgrid, but was %v", len(candidates))
 	}
 }
