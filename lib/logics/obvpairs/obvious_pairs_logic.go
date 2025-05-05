3703128573a2512a634a1f7ce15f7c11910fcae1
David England <david.england.de@googlemail.com>
2025-05-05 09:20:52 +0100

    Solve obvious pairs in column

--- lib/logics/obvpairs/obvious_pairs_logic.go
+++ lib/logics/obvpairs/obvious_pairs_logic.go
@@ -3,8 +3,9 @@ package obvpairs
 import "solver-zero/lib"
 
 type ObviousPairsLogic struct {
-	Sudoku     *lib.Sudoku
-	pairsInRow []pairInRow
+	Sudoku        *lib.Sudoku
+	pairsInRow    []pairInRow
+	pairsInColumn []pairInColumn
 }
 
 type pairInRow struct {
@@ -13,16 +14,30 @@ type pairInRow struct {
 	cells [2]lib.Coords
 }
 
+type pairInColumn struct {
+	nums   [2]int
+	column int
+	cells  [2]lib.Coords
+}
+
 func (logic *ObviousPairsLogic) RunStep() (bool, error) {
 	logic.pairsInRow = make([]pairInRow, 0)
+	logic.pairsInColumn = make([]pairInColumn, 0)
 
 	for row := 0; row < 9; row++ {
 		logic.resolveRow(row)
+	}
+	for col := 0; col < 9; col++ {
+		logic.resolveColumn(col)
 	}
 
 	for _, pair := range logic.pairsInRow {
 		cols := [2]int{pair.cells[0].ColumnIndex, pair.cells[1].ColumnIndex}
 		banRowExcept(pair.nums, cols, pair.row, logic.Sudoku)
+	}
+	for _, pair := range logic.pairsInColumn {
+		rows := [2]int{pair.cells[0].RowIndex, pair.cells[1].RowIndex}
+		banColumnExcept(pair.nums, rows, pair.column, logic.Sudoku)
 	}
 
 	return false, nil
@@ -57,9 +72,48 @@ func (logic *ObviousPairsLogic) resolveRow(row int) {
 	}
 }
 
+func (logic *ObviousPairsLogic) resolveColumn(col int) {
+	cellsWith2 := make(map[int][2]int)
+
+	for row := 0; row < 9; row++ {
+		if cand := logic.Sudoku.CandidateNumbers(row, col); len(cand) == 2 {
+			candSortedArr := sort(([2]int)(cand))
+
+			for k, v := range cellsWith2 {
+				if candSortedArr == v {
+
+					obviousPair := pairInColumn{
+						nums:   candSortedArr,
+						column: col,
+						cells: [2]lib.Coords{
+							{RowIndex: k, ColumnIndex: col},
+							{RowIndex: row, ColumnIndex: col},
+						},
+					}
+
+					logic.pairsInColumn = append(logic.pairsInColumn, obviousPair)
+					break
+				}
+			}
+
+			cellsWith2[row] = candSortedArr
+		}
+	}
+}
+
 func banRowExcept(numsToBan, exceptCols [2]int, row int, sudoku *lib.Sudoku) {
 	for col := 0; col < 9; col++ {
 		if !(col == exceptCols[0] || col == exceptCols[1]) {
+			for _, num := range numsToBan {
+				sudoku.Ban(num, lib.Coords{RowIndex: row, ColumnIndex: col})
+			}
+		}
+	}
+}
+
+func banColumnExcept(numsToBan, exceptRows [2]int, col int, sudoku *lib.Sudoku) {
+	for row := 0; row < 9; row++ {
+		if !(row == exceptRows[0] || row == exceptRows[1]) {
 			for _, num := range numsToBan {
 				sudoku.Ban(num, lib.Coords{RowIndex: row, ColumnIndex: col})
 			}
